dadd85b934089d26f3352e453eb49589586fb053
Christine Maynard <christine.ri.maynard@gmail.com>
2022-12-11 23:15:56 -0500

    Add class to create a file with user info

---
+++ src/main/java/CreateUserFile.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+import com.github.javafaker.Faker;
+
+public class CreateUserFile {
+    static Faker faker = new Faker();
+    public void CreateUserFile() {
+        this.faker = faker;
+    }
+    public static  void main(String[] args) throws IOException {
+
+        String userFileName = faker.funnyName().name();
+        String filePath =  "/Users/christinemaynard/Documents/filehandling//" + userFileName + ".txt";
+        String userName = String.valueOf(faker.name().fullName());
+        String userProfession = faker.job().title();
+        Integer userId = faker.random().hashCode();
+        // faker generates some names with whitespace so we use replaceAll to remove the white spaces
+        if(userFileName.length() > 1){
+           filePath = filePath.replaceAll("\\s+", "");
+        }
+        try {
+            FileWriter createProfile = new FileWriter(filePath);
+            // TODO - have each section write to a new line instead of one line
+            createProfile.write("User id: " + userId);
+            createProfile.write(" Username: " + userName);
+            createProfile.write(" User profession " + userProfession);
+            createProfile.close();
+            System.out.println("User profile created");
+
+        }   // Catch block to handle if exception occurs
+        catch (IOException e) {
+            System.out.print(e.getMessage());
+        }
+
+    }
+}
